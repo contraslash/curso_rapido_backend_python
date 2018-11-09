65c287488cca632f4e0ecc10750975dea1628d88
ma0 <ma0@contraslash.com>
2018-11-09 15:10:39 -0500

    improved code structure of example

--- ejercicios/clase_02/hello_world/hello_world_no_framework/hello_world.py
+++ ejercicios/clase_02/hello_world/hello_world_no_framework/hello_world.py
@@ -1,13 +1,13 @@
-from cgi import parse_qs, escape
+from io import StringIO
+from wsgiref.simple_server import make_server
 
 def hello_world(environ, start_response):
-    from io import StringIO
+
     stdout = StringIO()
     print("Hello world!", file=stdout)
     start_response("200 OK", [('Content-Type', 'text/plain; charset=utf-8')])
     return [stdout.getvalue().encode("utf-8")]
 
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
     srv = make_server('localhost', 8080, hello_world)
     srv.serve_forever()
