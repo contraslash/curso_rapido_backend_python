9695224bfe1cb8c7391df12bfbdbfbbafa89210a
Mauricio Collazos <ma0@contraslash.com>
2018-12-03 21:10:12 -0500

    added logic for upload file django

--- ejercicios/clase_10/upload_file/applications/core/views.py
+++ ejercicios/clase_10/upload_file/applications/core/views.py
@@ -1,3 +1,4 @@
+import csv
 from django.shortcuts import render
 from  django.views.generic import FormView
 
@@ -14,5 +15,16 @@ class SimpleUpload(FormView):
 
     def form_valid(self, form):
         print(form.cleaned_data)
+        archivo = form.cleaned_data.get("file")
+        # readed_file = archivo.read().decode()
+        # print(archivo.readlines())
+        binary_lines = archivo.readlines()
+        decoded_lines = [line.decode() for line in binary_lines]
+        posts = list()
+        reader = csv.reader(decoded_lines)
+        # Skip first line
+        next(reader)
+        for r in reader:
+            print(r)
 
         return super(SimpleUpload, self).form_valid(form=form)
